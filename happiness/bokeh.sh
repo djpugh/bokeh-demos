d9ea04c75e926b033ff537052ad37cb57570071d
David Pugh <djpugh@gmail.com>
2016-09-30 14:57:52 +0100

    Added fix for issue #25

--- happiness/bokeh.sh
+++ happiness/bokeh.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-bokeh serve --log-level=info --host=localhost:5006 --host=localhost:8001
+bokeh serve --log-level=info --host=localhost:5006 --host=localhost:8001 --allow-websocket-origin=127.0.0.1:8000
